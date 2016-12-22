7c6a43aebedd791c77ff775149d6e8cfbd17d63a
Arnór <arnoreids@gmail.com>
2016-12-22 18:03:00 +0000

    Shift_fail og fleira
    Bætti við nýju falli "Shift_fail" og setti forritið aðeins betur upp.

--- Skiptir/Skiptir.ino
+++ Skiptir/Skiptir.ino
@@ -1,75 +1,89 @@
 
-//  Ingangar
+//  Ingangar#####################
 const int GUPin = 2;
 const int GDPin = 3;
 const int NULLPin = 4;
 const int WentInGear = 5;
 const int WentBack = 6;
-//  Útgangar
+//  ï¿½tgangar#####################
 const int GUOut = 7;
 const int GDOut = 8;
 
-// Breytur
+// Breytur#####################
 
 int whatGear = 0;
 bool isChangingGear;
 
+//FÃ¶ll#####################
+void shift_fail();
 
 
+//#################  SETUP  #####################
 void setup() {
-	pinMode(GUPin, INPUT);
-	pinMode(GDPin, INPUT);
-	pinMode(NULLPin, INPUT);
-	pinMode(WentInGear, INPUT);
-	pinMode(WentBack, INPUT);
+  pinMode(GUPin, INPUT);
+  pinMode(GDPin, INPUT);
+  pinMode(NULLPin, INPUT);
+  pinMode(WentInGear, INPUT);
+  pinMode(WentBack, INPUT);
 
-	pinMode(GUOut, OUTPUT);
-	pinMode(GDOut, OUTPUT);
+  pinMode(GUOut, OUTPUT);
+  pinMode(GDOut, OUTPUT);
 
-	// initialize serial communications:
-	Serial.begin(9600);
+  // initialize serial communications:
+  Serial.begin(9600);
 }
 
+//#####################THE LOOP#####################
 void loop() {
 
 
 
-	//Change Gear upp
-	if (digitalRead(GUPin) == HIGH && !isChangingGear) {
+  //Change Gear upp #####################
+  if (digitalRead(GUPin) == HIGH && !isChangingGear) {
 
-		digitalWrite(GUOut, HIGH);
-		isChangingGear = true;
-		Serial.print("Skipti upp");
+    digitalWrite(GUOut, HIGH);
+    isChangingGear = true;
+    Serial.print("Skipti upp");
 
-	}
-	//Change gear down
-	if (digitalRead(GUPin) == HIGH && !isChangingGear) {
+  }
+  //Change gear down #####################
+  if (digitalRead(GUPin) == HIGH && !isChangingGear) {
 
-		digitalWrite(GDOut, HIGH);
-		isChangingGear = true;
-		Serial.print("Skipti niður");
+    digitalWrite(GDOut, HIGH);
+    isChangingGear = true;
+    Serial.print("Skipti niï¿½ur");
 
-	}
-	//Gear change sucessfull
-	if (isChangingGear && digitalRead(WentInGear) == HIGH) {
+  }
+  //Gear change sucessfull #####################
+  if (isChangingGear && digitalRead(WentInGear) == HIGH) {
 
-		if (GDOut == HIGH) { whatGear--; };
-		if (GUOut == HIGH) { whatGear++; };
+    if (GDOut == HIGH) { whatGear--; };
+    if (GUOut == HIGH) { whatGear++; };
 
-		digitalWrite(GDOut, LOW);
-		digitalWrite(GUOut, LOW);
-		isChangingGear = false;
+    digitalWrite(GDOut, LOW);
+    digitalWrite(GUOut, LOW);
+    isChangingGear = false;
 
-	}
+  }
 
 
-	Serial.print("Current gear: ");
-	Serial.print(whatGear);
-	Serial.println();
-	Serial.print("GearUp: ");
-	Serial.print(GUPin);
-	Serial.println();
+  Serial.print("Current gear: ");
+  Serial.print(whatGear);
+  Serial.println();
+  Serial.print("GearUp: ");
+  Serial.print(GUPin);
+  Serial.println();
 
 
 
 }
+//######################################## FÃ–LL #################################################
+//Skipting mistÃ³kst
+void shift_fail()
+{
+  isChangingGear = false;
+  digitalWrite(GUOut,LOW);
+  digitalWrite(GDOut,LOW);
+  Serial.println("### Gear failed to shift! ###");
+  return;
+};
